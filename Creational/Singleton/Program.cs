8c2a641fc8e5dd6c4b8717c561724d86491f7d51
przemekgwozdz <gw0zdziu09@gmail.com>
2026-09-09 01:11:51 +0200

    Add Singleton pattern implementation with example application

---
+++ Creational/Singleton/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Singleton;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var logger = Logger.GetInstance();
+        logger.Log("Hello World");
+        logger.Log("Hello World 2");
+        logger.Log("Hello World 3");
+        logger.PrintMessages();
+    }
+}
+
+public class Logger
+{
+    private static  Logger _logger;
+    private static readonly object padlock = new object();
+    private List<string> _messages = [];
+    
+    private Logger() { }
+    
+    public static Logger GetInstance()
+    {
+        lock (padlock)
+        {
+            if (_logger == null)
+            {
+                _logger = new Logger();
+            }
+            return _logger;
+        }
+    }
+    
+    public void Log(string message)
+    {
+        _messages.Add(message);
+    }
+
+    public void PrintMessages()
+    {
+        foreach (var message in _messages)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
