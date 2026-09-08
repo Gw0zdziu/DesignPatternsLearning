2f355acc7352b66ac6682d21f4011b7caefd88ef
przemekgwozdz <gw0zdziu09@gmail.com>
2026-09-09 01:12:26 +0200

    Add Prototype pattern implementation with example application

---
+++ Creational/Prototype/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Prototype;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var npcGame = new UnitNpcGame("Test", 100, 1);
+        var npcGame2 = npcGame.Clone();
+    }
+}
+
+public interface IPrototype
+{
+    public IPrototype Clone();
+}
+
+public class UnitNpcGame : IPrototype
+{
+    private string _name;
+    private int _level;
+    private int _health;
+
+    public UnitNpcGame(string name, int health, int level)
+    {
+        _name = name;
+        _health = health;
+        _level = level;
+    }
+
+
+    public IPrototype Clone()
+    {
+        var newNpcGame = new UnitNpcGame(_name, _health, _level);
+        return newNpcGame;
+    }
+    public override string ToString()
+    {
+        return $"{_name} {_level} {_health}";
+    }
+    
+}
