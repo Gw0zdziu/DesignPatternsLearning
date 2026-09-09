f475aea58ebf662230e69b924bcbeba3c100bef3
przemekgwozdz <gw0zdziu09@gmail.com>
2026-09-09 21:06:45 +0200

    Add Adapter pattern implementation with example application

---
+++ Structural/Adapter/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Adapter;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IPaymentGateway paypalAdapter = new BankTransferAdapter(new BankTransfer());
+       paypalAdapter.Pay(100);
+    }
+}
+
+public interface IBankTransfer
+{
+    public void MakeOldPayment(decimal amount);
+}
+
+public class BankTransfer : IBankTransfer
+{
+    public void MakeOldPayment(decimal amount)
+    {
+        Console.WriteLine($"Make old payment with bank transfer {amount}");
+    }
+}
+
+public interface IPaymentGateway
+{
+    public void Pay(decimal amount);
+}
+
+public class BankTransferAdapter : IPaymentGateway
+{
+    private readonly IBankTransfer _bankTransfer;
+
+    public BankTransferAdapter(IBankTransfer bankTransfer)
+    {
+        _bankTransfer = bankTransfer;
+    }
+
+    public void Pay(decimal amount)
+    {
+        _bankTransfer.MakeOldPayment(amount);
+    }
+}
+
+
+
