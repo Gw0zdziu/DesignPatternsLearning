dd210da8a9c90d888245e8a28bc4ffaddc08ad7f
przemekgwozdz <gw0zdziu09@gmail.com>
2026-09-09 21:06:56 +0200

    Add Facade pattern implementation with example application

---
+++ Structural/Facade/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Facade;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var orderFacade = new OrderFacade();
+        orderFacade.PlaceOrder();
+    }
+}
+
+public class OrderFacade
+{
+    private readonly Warehouse _warehouse = new Warehouse();
+    private readonly Payment _payment = new Payment();
+    private readonly Courier _courier = new Courier();
+
+    public void PlaceOrder()
+    {
+        _warehouse.StoreOrder();
+        _payment.ProcessPayment();
+        _courier.DeliverOrder();
+    }
+}
+
+internal class Courier
+{
+    public void DeliverOrder()
+    {
+        
+    }
+}
+
+internal class Payment
+{
+    public void ProcessPayment()
+    {
+        
+    }
+}
+
+internal class Warehouse
+{
+    public void StoreOrder()
+    {
+        
+    }
+}
