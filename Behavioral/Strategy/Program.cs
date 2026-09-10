21773e0e5792bf1c0d26e667e99c40d41d9fb6be
przemekgwozdz <gw0zdziu09@gmail.com>
2026-09-11 00:15:56 +0200

    Add Strategy pattern implementation with example application

---
+++ Behavioral/Strategy/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Strategy;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CalculatorDiscount discount = new CalculatorDiscount(new DiscountPercent());
+        Console.WriteLine(discount.Calculate(1000));
+        discount.SetStrategy(new DiscountAmount());
+        Console.WriteLine(discount.Calculate(1000));
+    }
+}
+
+public interface ICalculatorStrategy
+{
+    public decimal Calculate(decimal amount);
+}
+
+public class DiscountPercent: ICalculatorStrategy
+{
+    public decimal Calculate(decimal amount)
+    {
+        return amount - (amount * (decimal)0.1);
+    }
+}
+
+public class DiscountAmount: ICalculatorStrategy
+{
+    public decimal Calculate(decimal amount)
+    {
+        var price =  amount - 100;
+        if (price < 0)
+        {
+            return 0;
+        }
+        return price;
+    }
+}
+
+public class NoDiscount: ICalculatorStrategy
+{
+    public decimal Calculate(decimal amount)
+    {
+        return amount;
+    }
+}
+
+public class CalculatorDiscount
+{
+    private ICalculatorStrategy _strategy;
+    
+    public CalculatorDiscount(ICalculatorStrategy strategy)
+    {
+        _strategy = strategy;
+    }
+
+    public void SetStrategy(ICalculatorStrategy strategy)
+    {
+        _strategy = strategy;
+    }
+    
+    public decimal Calculate(decimal amount)
+    {
+        return _strategy.Calculate(amount);
+    }
+}
